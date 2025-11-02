205491152ff9c81d76fc53e683209f3f77156786
MoadVI <moadchess101@gmail.com>
2025-11-02 16:00:42 +0100

    Add vector and matrix operations to Math.py

--- Math/Math.py
+++ Math/Math.py
@@ -6,3 +6,15 @@ def PI(L):
         p *= L[i]
 
     return p
+
+def vec_add(self, u, v):
+    return [u[i] + v[i] for i in range(len(u))]
+
+def vec_sub(self, u, v):
+    return [u[i] - v[i] for i in range(len(u))]
+
+def vec_mul_scalar(self, u, s):
+    return [u[i] * s for i in range(len(u))]
+
+def mat_vec_mul(self, M, v):
+    return [sum(M[i][j] * v[j] for j in range(len(v))) for i in range(len(M))]
