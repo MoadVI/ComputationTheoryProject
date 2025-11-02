83e7b49e093f4d36ff1325ca96cca2769dfb8ed1
MoadVI <moadchess101@gmail.com>
2025-11-02 16:02:52 +0100

    Update Math.py
    Removed 'self' parameter from vector and matrix operations.

--- Math/Math.py
+++ Math/Math.py
@@ -7,14 +7,14 @@ def PI(L):
 
     return p
 
-def vec_add(self, u, v):
+def vec_add(u, v):
     return [u[i] + v[i] for i in range(len(u))]
 
-def vec_sub(self, u, v):
+def vec_sub(u, v):
     return [u[i] - v[i] for i in range(len(u))]
 
-def vec_mul_scalar(self, u, s):
+def vec_mul_scalar(u, s):
     return [u[i] * s for i in range(len(u))]
 
-def mat_vec_mul(self, M, v):
+def mat_vec_mul(M, v):
     return [sum(M[i][j] * v[j] for j in range(len(v))) for i in range(len(M))]
